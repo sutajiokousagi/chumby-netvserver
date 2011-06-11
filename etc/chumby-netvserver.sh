b7cb8d56250e0735b17836a8b5ed983b513624be
Sean Cross <sean@chumby.com>
2011-06-11 09:48:35 +0800

    chumby-netvserver: Add unified SysV startup script

---
+++ etc/chumby-netvserver.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+case "$1" in
+	start)
+		# Start in the background so we don't hog the console
+		# This app uses QtGui hence requires -qws option,
+		# but does not render anything to the framebuffer
+		NeTVServer -qws -nomouse 2>&1 2> /dev/null&
+		;;
+
+	stop)
+		killall NeTVServer 2> /dev/null
+		;;
+
+	*)
+
+		echo "Usage: $0 {start|stop}"
+		;;
+esac
