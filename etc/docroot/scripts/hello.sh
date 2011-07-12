ee6db4e1fb16b93f3ae6a3324b0e518627afe1e2
Torin <torin@chumby.com>
2011-07-12 18:13:50 +0800

    hello.sh: Use real GUID from new guidgen.sh script

--- etc/docroot/scripts/hello.sh
+++ etc/docroot/scripts/hello.sh
@@ -3,7 +3,12 @@
 # Returning identification information of this hardware
 #
 
-echo "<guid>A620123B-1F0E-B7CB-0E11-921ADB7BE22A</guid>"
+#Testing GUID
+#echo "<guid>A620123B-1F0E-B7CB-0E11-921ADB7BE22A</guid>"
+#echo "<guid>4FCD4051-9867-006A-9DC1-443BD3477E8E</guid>"
+GUID=$(guidgen.sh)
+
+echo "<guid>${GUID}</guid>"
 echo "<dcid>a long string of 1024 bytes</dcid>"
 echo "<hwver>10.1</hwver>"
 echo "<fwver>1.0</fwver>"
