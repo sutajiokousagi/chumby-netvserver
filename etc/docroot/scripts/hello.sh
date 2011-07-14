681b61fa47d8352f4eccacb76982b1c49c3892ff
Torin <torin@chumby.com>
2011-07-14 11:30:04 +0800

    hello.sh: Give demo GUIDE if guidgen.sh not present. Add <internet> tag

--- etc/docroot/scripts/hello.sh
+++ etc/docroot/scripts/hello.sh
@@ -3,13 +3,48 @@
 # Returning identification information of this hardware
 #
 
-#Testing GUID
-#echo "<guid>A620123B-1F0E-B7CB-0E11-921ADB7BE22A</guid>"
-#echo "<guid>4FCD4051-9867-006A-9DC1-443BD3477E8E</guid>"
+# Give demo GUID if no guidgen.sh
 GUID=$(guidgen.sh)
+if [ -n $GUID ]; then
+	GUID=A620123B-1F0E-B7CB-0E11-921ADB7BE22A
+fi
 
+# To be replaced by a actual script
+DCID=''
+if [ -n $DCID ]; then
+	DCID='a long string of 1024 bytes'
+fi
+
+# To be replaced by a dcid script
+hwver=''
+if [ -n $hwver ]; then
+	hwver='10.1'
+fi
+
+# To be replaced by a dcid script
+fwver=''
+if [ -n $fwver ]; then
+	fwver='1.0'
+fi
+
+# No flash plugin on NeTV
+flashplugin='0'
+
+# Network status
+network_status=$(network_status.sh)
+internet='false'
+hasLinkTrue=$(echo network_status | grep 'link="true"')
+if [ -n $hasLinkTrue ]; then
+	internet='true'
+fi
+
+#
+# Print them out
+#
 echo "<guid>${GUID}</guid>"
-echo "<dcid>a long string of 1024 bytes</dcid>"
-echo "<hwver>10.1</hwver>"
-echo "<fwver>1.0</fwver>"
-echo "<flashplugin>0</flashplugin>"
+echo "<dcid>$DCID</dcid>"
+echo "<hwver>$hwver</hwver>"
+echo "<fwver>$fwver</fwver>"
+echo "<flashplugin>$flashplugin</flashplugin>"
+echo "<internet>$internet</internet>"
+echo "$network_status"
