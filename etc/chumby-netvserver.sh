29c1218c58f9628ded7da011e9baee6814eb62db
Torin <torin@chumby.com>
2011-07-15 16:36:55 +0800

    temp Semop was in the wrong line

--- etc/chumby-netvserver.sh
+++ etc/chumby-netvserver.sh
@@ -14,13 +14,14 @@ case "$1" in
 		fpga_ctl w 0xe 0
 		fpga_ctl w 0xf 240
 		
-		# Temp fix for "Semop lock/unlock failure Identifier removed" flood
-		rm -rf /tmp/qtembedded-0
-
 		# start in the background so we don't hog the console
 		# This app uses QtGui hence requires -qws option,
 		# but does not render anything to the framebuffer
 		NeTVServer -qws -nomouse 2>&1 2> /dev/null&
+		
+		# Temp fix for "Semop lock/unlock failure Identifier removed" flood
+		# Should be after we started the Qt process
+		rm -rf /tmp/qtembedded-0
 
 		# fullscreen
 		setbox 0 0 1279 719
