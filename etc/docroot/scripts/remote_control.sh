c1a0f1c8110d8db262fe6ea03b279e8392e08211
Torin <torin@chumby.com>
2011-07-15 17:40:10 +0800

    remote_control.sh: hide the flashplayer when we reset

--- etc/docroot/scripts/remote_control.sh
+++ etc/docroot/scripts/remote_control.sh
@@ -6,13 +6,18 @@
 # Android/iOS remote: tbd
 #
 
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+
 cmd=$(echo $1 | tr '[A-Z]' '[a-z]')
 
 # Start
 if [ "$cmd" == "reset" ];
 then
 	# Hide the flash player
-	setplayer 0 0 1 1 
+	${SCRIPTPATH}/widget_engine.sh hide
 
 	# Minimize the browser first	
 	/usr/bin/NeTVBrowser -qws -nomouse Hide
