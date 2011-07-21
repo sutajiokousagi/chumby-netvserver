6dc1e88657c11516e8ee42af5b69773be00da490
Torin <torin@chumby.com>
2011-07-21 18:25:12 +0800

    Check if dbus connects properly

--- lib/bfDBusMonitor/src/dbusmonitor.cpp
+++ lib/bfDBusMonitor/src/dbusmonitor.cpp
@@ -4,11 +4,19 @@
 DBusMonitor::DBusMonitor(QObject *parent) : QObject(parent)
 {
     nm_interface = new org::freedesktop::NetworkManagerInterface("org.freedesktop.NetworkManager", "/", QDBusConnection::systemBus(), this);
-    qDebug("DBusMonitor: started");
 
-    QDBusConnection::sessionBus().connect("org.freedesktop.NetworkManager", "/", "org.freedesktop.NetworkManager", "StateChanged", this, SLOT(StateChanged(uint)));
-    QDBusConnection::sessionBus().connect("org.freedesktop.NetworkManager", "/", "org.freedesktop.NetworkManager", "DeviceAdded", this, SLOT(DeviceAdded(QDBusObjectPath)));
-    QDBusConnection::sessionBus().connect("org.freedesktop.NetworkManager", "/", "org.freedesktop.NetworkManager", "DeviceRemoved", this, SLOT(DeviceRemoved(QDBusObjectPath)));
+    if (!nm_interface->isValid())
+    {
+        qDebug("DBusMonitor: error");
+        qDebug("%s", qPrintable(QDBusConnection::sessionBus().lastError().message()));
+    }
+    else
+    {
+        QDBusConnection::sessionBus().connect(QString(), QString(), "org.freedesktop.NetworkManager", "StateChanged", this, SLOT(StateChanged(uint)));
+        QDBusConnection::sessionBus().connect(QString(), QString(), "org.freedesktop.NetworkManager", "DeviceAdded", this, SLOT(DeviceAdded(QDBusObjectPath)));
+        QDBusConnection::sessionBus().connect(QString(), QString(), "org.freedesktop.NetworkManager", "DeviceRemoved", this, SLOT(DeviceRemoved(QDBusObjectPath)));
+        qDebug("DBusMonitor: started");
+    }
 }
 
 DBusMonitor::~DBusMonitor()
