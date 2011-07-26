35c22f2e8d96da7c1bbc75a42b13dafbe34c1ebc
Torin <torin@chumby.com>
2011-07-26 13:48:02 +0800

    Add curl.sh script

---
+++ etc/docroot/scripts/curl.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#
+# $1 is the url + GET parameters (if any)
+# $2 is the POST data in format "param1=value1&param2=value2" (or blank if using GET)
+#
+
+content=''
+if [ -z $2 ]; then
+	content=$(curl $1)
+else
+	content=$(curl -d $2 $1)
+fi
+echo $content
