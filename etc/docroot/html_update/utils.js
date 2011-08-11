be5116b87c807addfadaed87a0f8a4b477c1ec64
Torin <torin@chumby.com>
2011-08-11 21:00:48 +0800

    same as previous

--- etc/docroot/html_update/utils.js
+++ etc/docroot/html_update/utils.js
@@ -60,7 +60,7 @@ function xmlhttpPost(
 	}
 	
 	if (vUrl == '')
-		vUrl = "./bridge";
+		vUrl = "../bridge";
 	xmlHttpReq.open(vType, vUrl, true);
 	xmlHttpReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	xmlHttpReq.onreadystatechange = function()
