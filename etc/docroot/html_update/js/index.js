4a4436b7b4a2a66279739bb0bd378ededa089f2a
Torin <torin@chumby.com>
2011-08-15 13:09:49 +0800

    Add progress bar to html_update. It is now simply a page to display spining icon & progress bar. No other app logic.

--- etc/docroot/html_update/js/index.js
+++ etc/docroot/html_update/js/index.js
@@ -1,3 +1,4 @@
+var main_x;
 var main_y;
 var updateCount;
 var needReboot;
@@ -7,29 +8,38 @@ function onLoad()
 	//Params passed thru GET
 	updateCount = GET('updateCount');
 	needReboot = GET('reboot');
+	continue_percentage = GET('continue');
 	
-	//Hide everything immediately, animate in later
+	//Save initial parameters
 	main_y = $("#div_center").offset().top;
-	main_hideMainPanel(50);
-	
+	main_x = $("#div_center").offset().left;
 	$("#div_loadingMain").fadeIn(0);
 	
-	setTimeout("onLoadLater()", 100);
+	setUpgradeProgress(continue_percentage);
+	
+	//Not continue from an upgrade process
+	if (continue_percentage == "" || continue_percentage == 0)
+	{
+		fDbg2("Start upgrading UI");
+		
+		//Hide everything immediately, animate in later
+		main_hideMainPanel();
+	
+		setTimeout("onLoadLater()", 100);
+	}
+	
+	//This happens when browser restarts half way during an upgrading process
+	else
+	{
+		fDbg2("Continue upgrading UI: " + continue_percentage + "%");
+		main_showMainPanel();
+	}
 }
 
 function onLoadLater()
 {
 	//Gracefully show the update panel
-	main_showMainPanel();
-	
-	//Start heavy updating after animation is done
-	setTimeout("doUpgrade()", 1650);
-}
-
-function doUpgrade()
-{
-	//Call systemupdate.sh script
-	xmlhttpPost("http://localhost/bridge", "post", { 'cmd' : 'SystemUpdate', 'value' : '' }, updateDoneCallback );
+	main_showMainPanel(1650);
 }
 
 //-----------------------------------------------------------
@@ -37,12 +47,21 @@ function doUpgrade()
 function main_showMainPanel(duration)
 {
 	$("#div_center").css('visibility', 'visible');
-	$("#div_center").animate({ top: main_y }, !duration ? 1600 : duration);
+	
+	var offset = $("#div_center").offset();
+	offset.top = main_y;
+	
+	if (!duration || duration == 0)		$("#div_center").offset( offset );
+	else								$("#div_center").animate({ top: main_y }, !duration ? 1600 : duration);
 }
 
 function main_hideMainPanel(duration)
 {
-	$("#div_center").animate({ top: main_y + $(window).height() }, !duration ? 1600 : duration);
+	var offset = $("#div_center").offset();
+	offset.top = $(window).height();
+	
+	if (!duration || duration == 0)		$("#div_center").offset( offset );
+	else								$("#div_center").animate({ top: main_y + $(window).height() }, !duration ? 1600 : duration);
 }
 
 //-----------------------------------------------------------
@@ -53,31 +72,60 @@ function GET( paramName )
 	var regexS = "[\\?&]"+paramName+"=([^&#]*)";
 	var regex = new RegExp( regexS );
 	var results = regex.exec( window.location.href );
-	if( results == null )    return "";
-	else					   return results[1];
+	if( results == null )    	return "";
+	else					   	return results[1];
 }
 
-function updateDoneCallback( vData )
+//-----------------------------------------------------------
+
+function fUPDATEEvents( vEventName, vEventData )
 {
-	fDbg2("-------------------------------------------");
-	fDbg2("  Upgrade Done");
-	fDbg2("-------------------------------------------");
-	//fDbg2( vData );
+	switch (vEventName)
+	{
+		case "progress":	setUpgradeProgress(vEventData);		break;
+		case "done":		setUpgradeDone();					break;
+	}
+}
+
+function setUpgradeProgress(percentage)
+{
+	if (!percentage || percentage == "" || percentage == 0)
+		percentage = 0;
+	percentage = parseFloat(""+percentage);
+		
+	//Set the progress bar (very convenient!)
+	$("#progress_bar").width(""+percentage+"%");
+	
+	//Nicely style the end cap
+	if (percentage > 100-(24-8)/2)
+	{
+		percentage = Math.round(percentage);
+		$("#progress_bar").css("border-top-right-radius", 24-(100-percentage)*2);
+		$("#progress_bar").css("border-bottom-right-radius", 24-(100-percentage)*2);
+	}
+	else
+	{
+		$("#progress_bar").css("border-top-right-radius", 8);
+		$("#progress_bar").css("border-bottom-right-radius", 8);
+	}
+}
+
+function setUpgradeDone()
+{
+	//Hehe
+	setUpgradeProgress(100);
 	
 	//Gracefully hide the update panel
-	main_hideMainPanel();
+	main_hideMainPanel(1650);
 	
 	//May not reach here if upgrade script reboots the system itself
 	setTimeout("location.href=\"http://localhost/\"", 1650);
 }
 
-//-----------------------------------------------------------
 
 
 
-
-
-// System events are totally ignored below
+// Other system events are totally ignored below
 
 
 
@@ -135,11 +183,6 @@ function fNMDeviceRemoved(  )
 //	events from system
 // -------------------------------------------------------------------------------------------------
 function fUPDATECOUNTEvent( vEventData )
-{
-	return "command ignored";
-}
-
-function fUPDATEREADYEvent( vEventData )
 {
 	return "command ignored";
 }
--- etc/docroot/scripts/systemupdate.sh
+++ etc/docroot/scripts/systemupdate.sh
@@ -1,18 +1,46 @@
 #!/bin/sh
 #
 
+#
+# This script is deprecated.
+# All upgrading operation is now controlled by NeTVBrowser directly
+#
+
+#Get the list of upgradable packages
+if [ "$1" == "list-upgradable" ]; then
+	upgradeOuput=$(opkg list-upgradable)
+	echo "<outputlog>${upgradeOuput}</outputlog>"
+	exit
+	# Example output:
+	# chumby-netvserver - 1.0-r76.9 - 1.0-r77.9
+	# angstrom-version - v20110703-r18.9 - v20110703-r19.9
+	# chumby-netvbrowser - 1.0-r36.9 - 1.0-r37.9
+	# netv-controlpanel - 1.0-r30.9 - 1.0-r31.9
+fi
+
+#Get the size of all packages in /var/lib/opkg/tmp
+if [ "$1" == "list-size" ]; then
+	FILES=/var/lib/opkg/tmp/*.ipk
+	for filename in $FILES
+	do
+		filesizebyte=$(stat -c %s $filename)
+		echo "<file><filename>${filename}</filename><filesize>${filesizebyte}</filesize></file>"
+	done
+	exit
+fi
+
+
 # The downloading should have been done before this is triggered
-echo "executing opkg upgrade...";
 mount -o remount,rw /
 upgradeOuput=$(opkg --cache /var/lib/opkg/tmp upgrade)
 mount -o remount,ro /
 
-# Software version
+# Software version (build number)
 fwver=$(chumby_version -f)
 if [ -z "${fwver}" ]; then
 	fwver='1.0'
 fi
 
 # Output to web service
-#echo "<fwver>${fwver}</fwver>"
-#echo "<log>${upgradeOuput}</log>"
+echo "<fwver>${fwver}</fwver>"
+#echo "<outputlog>${upgradeOuput}</outputlog>"
