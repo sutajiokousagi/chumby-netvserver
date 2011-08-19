4bba34c503e645c5d5020d9228804887c6143ded
Torin <torin@chumby.com>
2011-08-19 12:55:10 +0800

    set time script handles timezone

--- etc/docroot/scripts/set_time.sh
+++ etc/docroot/scripts/set_time.sh
@@ -3,16 +3,20 @@
 #
 # $1 is time formatted in GMT time as "yyyy.mm.dd-hh:mm:ss"
 # $2 is standard timezone ID string formated as "Asia/Singapore"
+# see http://developer.android.com/reference/java/util/TimeZone.html#getID()
 #
 
 time=$1
 timezone=$2
 
-# Set time (this will print out the time)
-date -s $time
-date
-echo $1
-echo $2
+# Set timezone (non-volatile)
+if [ -e /usr/share/zoneinfo/$2 ];
+then
+	cp /usr/share/zoneinfo/$2 /psp/localtime
+fi
 
-# Set timezone
-#/usr/chumby/scripts/time_zone.sh $timezone		#not built yet
+# Set time (volatile)
+datetime=$(date -s $time)
+
+#Print output
+echo "<value>${datetime}</value>"
