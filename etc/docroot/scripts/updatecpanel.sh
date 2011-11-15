b74283d67e408d85bae7b490589dabc704e49466
Torin <torin@chumby.com>
2011-11-15 11:47:26 +0800

    update git-repo script has to create a symlink to /tmp for photo sharing feature to work

--- etc/docroot/scripts/updatecpanel.sh
+++ etc/docroot/scripts/updatecpanel.sh
@@ -8,11 +8,27 @@ GITPATH=http://git.chumby.com.sg/git/chumby-sg/netv-controlpanel.git
 echo "<docroot>${DOCROOT}/docroot</docroot>"
 echo "<gitoutput>"
 
+# If repo already exist, check if it is valid repo by a hard reset
+if [ -e "${DOCROOT}/docroot" ];
+then
+	cd ${DOCROOT}/docroot
+	if ! git reset --hard HEAD;
+	then
+		rm -rf "${DOCROOT}/docroot"
+	fi
+fi
+
 # Clone the repo if it doesn't exist
 if [ ! -e "${DOCROOT}/docroot" ];
 then
 	cd ${DOCROOT}
-	if git clone ${GITPATH} docroot; then
+	if git clone ${GITPATH} docroot;
+	then
+		# create symlink for /tmp
+		if [ ! -e ${DOCROOT}/docroot/tmp ]; then
+			mkdir ${DOCROOT}/docroot/tmp
+		fi
+		ln -sf /tmp ${DOCROOT}/docroot/tmp/netvserver
 		exit 0;
 	fi
 	if [ ! -e "${DOCROOT}/docroot" ]; then
@@ -23,16 +39,19 @@ then
 	exit 1;
 fi
 
-# Pull latest changes, discard any local changes made
-cd "${DOCROOT}/docroot"
-git reset --hard HEAD
+# Exists, just do a pull
 if git pull; then
+	# create for /tmp
+	if [ ! -e ${DOCROOT}/docroot/tmp ]; then
+		mkdir ${DOCROOT}/docroot/tmp
+	fi
+	ln -sf /tmp ${DOCROOT}/docroot/tmp/netvserver
 	echo "</gitoutput>"
 	exit 0;
 fi
 
 # Failed halfway, rollback the changes
-git reset --hard HEAD
+rm -rf "${DOCROOT}/docroot"
 echo "Error updating cpanel git repo"
 echo "</gitoutput>"
 exit 1;
