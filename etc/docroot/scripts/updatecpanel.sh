a28b8bf1e13c0c6c131aaae5e62fe602f8b3c539
Torin <torin@chumby.com>
2011-11-17 16:35:38 +0800

    docroot git repo now points to github, and fallback to chumby.com.sg if it fails

--- etc/docroot/scripts/updatecpanel.sh
+++ etc/docroot/scripts/updatecpanel.sh
@@ -1,57 +1,91 @@
-#!/bin/sh
+#!/bin/bash
 
-# This is in home folder of root account in NeTV, always writable
-# Returns (echo) the path to caller so that we can change the path just by changing this script
-DOCROOT=/media/storage
-GITPATH=http://git.chumby.com.sg/git/chumby-sg/netv-controlpanel.git
+# The folder /media/storage is always writable
+# Returns the successful absolute path to docroot to caller
 
-echo "<docroot>${DOCROOT}/docroot</docroot>"
-echo "<gitoutput>"
+DOCROOT_PATH=/media/storage
+GITPATH1=http://github.com/sutajiokousagi/netv-controlpanel.git
+GITPATH2=http://git.chumby.com.sg/git/chumby-sg/netv-controlpanel.git
 
-# If repo already exist, check if it is valid repo by a hard reset
-if [ -e "${DOCROOT}/docroot" ];
-then
-	cd ${DOCROOT}/docroot
-	if ! git reset --hard HEAD;
+
+# Function
+do_update()
+{
+	GITPATH=$1
+	DOCROOT=$2
+	
+	# If repo already exist, check if it is valid repo by comparing path & perform a hard reset
+	if [ -e "${DOCROOT_PATH}/${DOCROOT}" ];
 	then
-		rm -rf "${DOCROOT}/docroot"
+		# Check if the existing repo path matches the desired repo path
+		# This is in case we change the order of the repo path
+		repo_path=$(cat ${DOCROOT_PATH}/${DOCROOT}/.git/config | grep ${GITPATH})
+		if [ -z "${repo_path}" ];
+		then
+			rm -rf "${DOCROOT_PATH}/${DOCROOT}"
+		else
+			#Hard reset
+			cd ${DOCROOT_PATH}/${DOCROOT}
+			if ! git reset --hard HEAD;
+			then
+				rm -rf "${DOCROOT_PATH}/${DOCROOT}"
+			fi
+		fi		
 	fi
-fi
 
-# Clone the repo if it doesn't exist
-if [ ! -e "${DOCROOT}/docroot" ];
-then
-	cd ${DOCROOT}
-	if git clone ${GITPATH} docroot;
+	# Clone the repo if it doesn't exist
+	if [ ! -e "${DOCROOT_PATH}/${DOCROOT}" ];
 	then
-		# create symlink for /tmp
-		if [ ! -e ${DOCROOT}/docroot/tmp ]; then
-			mkdir ${DOCROOT}/docroot/tmp
+		cd ${DOCROOT_PATH}
+		if git clone ${GITPATH} ${DOCROOT};
+		then
+			# create symlink for /tmp
+			if [ ! -e ${DOCROOT_PATH}/${DOCROOT}/tmp ]; then
+				mkdir ${DOCROOT_PATH}/${DOCROOT}/tmp
+			fi
+			ln -sf /tmp ${DOCROOT_PATH}/${DOCROOT}/tmp/netvserver
+			return 0;
 		fi
-		ln -sf /tmp ${DOCROOT}/docroot/tmp/netvserver
-		exit 0;
-	fi
-	if [ ! -e "${DOCROOT}/docroot" ]; then
-		rm -rf "${DOCROOT}/docroot"
-	fi
-	echo "Error updating cpanel git repo"
-	echo "</gitoutput>"
-	exit 1;
-fi
 
-# Exists, just do a pull
-if git pull; then
-	# create for /tmp
-	if [ ! -e ${DOCROOT}/docroot/tmp ]; then
-		mkdir ${DOCROOT}/docroot/tmp
+		# Network error?
+		if [ ! -e "${DOCROOT_PATH}/${DOCROOT}" ]; then
+			rm -rf "${DOCROOT_PATH}/${DOCROOT}"
+		fi
+		echo "Error updating cpanel git repo"
+		return 1;
 	fi
-	ln -sf /tmp ${DOCROOT}/docroot/tmp/netvserver
-	echo "</gitoutput>"
+
+	# Exists, just do a pull
+	if git pull; then
+		# create for /tmp
+		if [ ! -e ${DOCROOT_PATH}/${DOCROOT}/tmp ]; then
+			mkdir ${DOCROOT_PATH}/${DOCROOT}/tmp
+		fi
+		ln -sf /tmp ${DOCROOT_PATH}/${DOCROOT}/tmp/netvserver
+		return 0;
+	fi
+
+	# Failed due to network error?
+	#rm -rf "${DOCROOT_PATH}/${DOCROOT}"
+
+	echo "Error updating cpanel git repo"
+	return 1;
+}
+
+
+
+OUTPUT=$(do_update ${GITPATH1} "docroot")
+if [ "$?" -eq "0" ]; then
+	echo "<docroot>${DOCROOT_PATH}/docroot</docroot>"
+	echo "<gitoutput>${OUTPUT}</gitoutput>"
 	exit 0;
 fi
 
-# Failed halfway, rollback the changes
-rm -rf "${DOCROOT}/docroot"
-echo "Error updating cpanel git repo"
-echo "</gitoutput>"
+OUTPUT=$(do_update ${GITPATH2} "docroot2")
+if [ "$?" -eq "0" ]; then
+	echo "<docroot>${DOCROOT_PATH}/docroot2</docroot>"
+	echo "<gitoutput>${OUTPUT}</gitoutput>"
+	exit 0;
+fi
+
 exit 1;
