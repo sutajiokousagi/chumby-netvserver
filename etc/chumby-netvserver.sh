22b7c794a9d6de5b3941faad34f3416d30385adb
Torin <torin@chumby.com>
2012-02-05 21:43:55 +0800

    startup script should check /psp/homepage before starting NeTVServer

--- etc/chumby-netvserver.sh
+++ etc/chumby-netvserver.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
-
 case "$1" in
 	start)
-		# [Temp]
-		$(ifconfig lo up)
+		# Check /psp/homepage and set docroot accordingly
+		/usr/share/netvserver/docroot/scripts/psphomepage.sh >> /var/log/cron_psphomepage.log 2>&1
 
 		# start in the background so we don't hog the console
 		NeTVServer > /dev/null 2>&1 &
 		;;
 
 	stop)
-		killall NeTVServer > /dev/null 2>&1 &
+		killall NeTVServer
 		;;
 
 	restart)
 		$0 stop
+		sleep 1
 		$0 start
 		;;
 
 	*)
-
 		echo "Usage: $0 {start|stop|restart}"
 		;;
 esac
