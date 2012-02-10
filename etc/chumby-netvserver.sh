43f6c9bbd643a8fa641cd01bd5889405853a1d53
Torin <torin@chumby.com>
2012-02-10 23:35:14 +0800

    chumby-netvserver start script to start lighttpd if it's not started yet

--- etc/chumby-netvserver.sh
+++ etc/chumby-netvserver.sh
@@ -5,6 +5,11 @@ case "$1" in
 		# Check /psp/homepage and set docroot accordingly
 		/usr/share/netvserver/docroot/scripts/psphomepage.sh >> /var/log/cron_psphomepage.log 2>&1
 
+		# Start lighttpd if it's not started yet
+		if [ -z "$(pidof lighttpd)" ]; then
+			/etc/init.d/lighttpd start
+		fi
+
 		# start in the background so we don't hog the console
 		NeTVServer > /dev/null 2>&1 &
 		;;
